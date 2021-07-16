aded1c81ffc2e5551343ce34bfd5429bf88c3d2c
dwep_cc <angelinos257@gmail.com>
2021-07-16 10:02:38 -0500

    Update config.php

--- Config/config.php
+++ Config/config.php
@@ -1,11 +1,8 @@
 <?php
     define("HOST",'locaLhost');
-    define("DB_USER",'u389267718_prestaya');
-    //define("DB_USER",'root');
-    define("DB_PASS",'J>+Y70Yc['); 
-    //define("DB_PASS",'');
-    define("DB_NAME",'u389267718_prestaya');
-    //define("DB_NAME",'prestacenter');
+    define("DB_USER",'root');
+    define("DB_PASS",'');
+    define("DB_NAME",'prestacenter');
 
     define("PORT",3306);
     define("CHARSET",'utf8');
